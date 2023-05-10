1e86ebe9ee97570adb1521b361b30b6c4d13334f
lectures202x <lectures.202x@gmail.com>
2023-05-10 18:06:12 +0900

    Update ChattingSystem_NetworkManager_Chat.cs

--- Assets/week12A_ChattingSystem/ChattingSystem_NetworkManager_Chat.cs
+++ Assets/week12A_ChattingSystem/ChattingSystem_NetworkManager_Chat.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
-using UnityEngine.UI;
 using TMPro;
 
 public class ChattingSystem_NetworkManager_Chat : MonoBehaviourPunCallbacks
